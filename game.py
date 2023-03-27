da4ce0603d9f4e137aa387812b1a0265362ded01
TimoKuukka <46355@edu.raseko.fi>
2023-03-27 15:13:14 +0300

    lisätty taustakuva ja laitettu se liikkuvaksi

--- game.py
+++ game.py
@@ -24,12 +24,23 @@ class Game:
             pygame.transform.rotozoom(x, 0, 1/16)
             for x in bird_imgs
         ]
+        bg_imgs = [
+            pygame.image.load(f"images/background/layer_{i}.png")
+            for i in [1, 2, 3]
+        ]
+        self.bg_imgs = [
+            pygame.transform.rotozoom(x, 0, 600 / x.get_height()).convert_alpha()
+            for x in bg_imgs
+        ]
 
 
     def init_objects(self):
         self.bird_y_speed = 0
         self.bird_pos = (200, 000)
         self.bird_lift = False
+        self.bg0_pos = 0
+        self.bg1_pos = 0
+        self.bg2_pos = 0
 
     def run(self):
         clock = pygame.time.Clock()
@@ -58,6 +69,10 @@ class Game:
                     self.bird_lift = False
 
     def handle_game_logic(self):
+        self.bg0_pos -= 0.25
+        self.bg1_pos -= 0.5
+        self.bg2_pos -= 2
+        
         bird_y = self.bird_pos[1]
 
         if self.bird_lift:
@@ -67,8 +82,6 @@ class Game:
         else:
             # Painovoima (lisää putoamisnopeutta joka kuvassa)
             self.bird_y_speed += 0.2
-
-        
 
         # Estetään lintua menemästä yli pelialueen yläreunan
         if bird_y < 0:
@@ -95,6 +108,15 @@ class Game:
     def update_screen(self):
         # Täytä tausta vaaleansinisellä
         self.screen.fill((230, 230, 255))
+
+        # piirrä taustakuvat
+        self.screen.blit(self.bg_imgs[0], (self.bg0_pos, 0))
+        self.screen.blit(self.bg_imgs[0], (self.bg0_pos + 800, 0))
+        self.screen.blit(self.bg_imgs[1], (self.bg1_pos, 0))
+        self.screen.blit(self.bg_imgs[1], (self.bg1_pos + 800, 0))
+        self.screen.blit(self.bg_imgs[2], (self.bg2_pos, 0))
+        self.screen.blit(self.bg_imgs[2], (self.bg2_pos + 800, 0))
+        
 
         # Piirrä lintu
         angle = -90 * 0.04 * self.bird_y_speed
